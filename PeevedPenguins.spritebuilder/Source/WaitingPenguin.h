0b274c51f953d2c3690e80151d3a7c69c1723914
Sylar12 <pinchaowang0422@gmail.com>
2015-01-29 13:44:10 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.h
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.h
+//  PeevedPenguins
+//
+//  Created by PinchaoWang on 15/1/29.
+//  Copyright (c) 2015年 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface WaitingPenguin : CCSprite
+
+@end
