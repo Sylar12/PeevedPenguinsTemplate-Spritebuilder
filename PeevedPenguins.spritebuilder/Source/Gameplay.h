b1d727d871e651f4f0a91525d252fbb71ecd37cf
Sylar12 <pinchaowang0422@gmail.com>
2015-01-28 18:52:27 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by PinchaoWang on 15/1/28.
+//  Copyright (c) 2015年 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Gameplay : CCSprite
+
+@end
