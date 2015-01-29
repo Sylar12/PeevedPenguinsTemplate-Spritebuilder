85878575768dcacf0c31e487382c0d07f91d1d5e
Sylar12 <pinchaowang0422@gmail.com>
2015-01-29 01:46:37 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -30,7 +30,7 @@
 }
 
 // called on every touch in this scene
--(void) touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+-(void) touchBegan:(CCTouch *)touch withEvent:(CCTouchEvent *)event
 {
     CGPoint touchLocation = [touch locationInNode:_contentNode];
     
@@ -70,7 +70,7 @@
     [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
 }
 
-- (void)touchMoved:(UITouch *)touch withEvent:(UIEvent *)event
+- (void)touchMoved:(CCTouch *)touch withEvent:(CCTouchEvent *)event
 {
     // whenever touches move, update the position of the mouseJointNode to the touch position
     CGPoint touchLocation = [touch locationInNode:_contentNode];
@@ -86,13 +86,13 @@
     }
 }
 
--(void) touchEnded:(UITouch *)touch withEvent:(UIEvent *)event
+-(void) touchEnded:(CCTouch *)touch withEvent:(CCTouchEvent *)event
 {
     // when touches end, meaning the user releases their finger, release the catapult
     [self releaseCatapult];
 }
 
--(void) touchCancelled:(UITouch *)touch withEvent:(UIEvent *)event
+-(void) touchCancelled:(CCTouch *)touch withEvent:(CCTouchEvent *)event
 {
     // when touches are cancelled, meaning the user drags their finger off the screen or onto something else, release the catapult
     [self releaseCatapult];
